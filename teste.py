c88fe29b7a4f0c0a81fd57cef25685034c107872
kayque791 <kayque32045@gmail.com>
2022-10-05 19:32:44 -0300

    primeiro arquivo

---
+++ teste.py
@@ -0,0 +1,12 @@
+numero_texto = 50
+numero = int(numero_texto)
+print('ola, vamos ao teste')
+print('-------------------')
+print('tenho umnumero guardaado em minha memoria, tnete advinhar')
+chute = int(input('para isso , digite um numero'))
+print('--------')
+if chute == numero:
+    print('vc acertouuu, cagada :-)')
+else:
+        print('eu ja esperava')
+print('boa sorte na proxima filhão')
